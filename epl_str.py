7057eb0752e5652cd905ba35d2c62b28d39e5341
mra49 <87374463+mra49@users.noreply.github.com>
2023-10-01 23:08:08 +0300

    Update epl_str.py

--- epl_str.py
+++ epl_str.py
@@ -15,16 +15,14 @@ def home(df):
     # Showcasing the data only first 23 rows
     st.write(df.head(23))
 
-# # Title for 1st graph
-# def Value_Age(df):
-#     st.subheader("Market Value Relative to Age")
-#     Fig_1 = px.scatter(df, x="age", y="market_value", size="fpl_points", hover_name="name", color="club")
-#     st.plotly_chart(Fig_1)
+
+    
+
 
 # Title for 1st graph
 def Value_Age(df):
     st.subheader("Market Value Relative to Age")
-    
+    st.write("this graph shows us how players market value change with their positions which can hint to the most rare postitions.")
     # Explanation for legend interactions
     st.write("Double-click on legend items to choose/un-choose teams.")
     
@@ -35,6 +33,7 @@ def Value_Age(df):
 # Title for second graph
 def Position_Age(df):
     st.subheader("Positions For Every Age")
+    st.write("This graph can show us which age group dominates every position")
     Fig_2 = px.scatter(df, x="age", y="position")
     # Allow the user to select a plot color
     col = st.color_picker("Select a plot color")
@@ -45,6 +44,7 @@ def Position_Age(df):
 # Third graph
 def Value_Position(df):
     st.subheader("Market Value According to Positions")
+    st.write("This graph gives options to explore how several variables affect the market value of players")
     x_axis = st.selectbox("Select X-axis Value", options=df.columns)
     y_axis = st.selectbox("Select Y-axis Value", options=df.columns)
     plot = px.scatter(df, x=x_axis, y=y_axis)
@@ -54,10 +54,12 @@ def Value_Position(df):
 def main_findings(df):
     st.subheader("Main Findings")
     # Add your main findings here
-    finding1 = '''Finding 1: If players want to increase there career span at the top level,
-             they have to adapt their style of play to occupy more defensive role which can be seen from the second scatter plot.'''
-    finding2 = '''Finding 2: The best time to sell players is when they are approaching the 28 years mark since this 
+    finding1 = '''Finding 1: The best time to sell players is when they are approaching the 28 years mark since this 
     is where their prices soar, the most valuable players aren't the ones that are too young, but the ones who have matured 
+    finding2 = '''Finding 2: If players want to increase there career span at the top level,
+             they have to adapt their style of play to occupy more defensive role which can be seen from the second scatter plot.'''
+    finding3 = ''' '''
+    
     and still are performing at their best (check 1st visual )'''    
     st.write(finding1)
     st.write(finding2)
